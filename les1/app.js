2b8782dd1fd975138e42e69e70435b3d421d2309
mick <34738@ma-web.nl>
2022-11-29 10:57:11 +0100

    first commit

---
+++ les1/app.js
@@ -0,0 +1,24 @@
+class App
+{
+    runapplication()
+    {
+    
+    let appNaam = "snapchat"
+    let versienummer = 0.7
+    let versiedatum = "dinsdag"
+    let autheur = "mick"
+    let copyright = 'altjd'
+    let distributeur = 'mora'
+    let darkmode = true
+    console.log(appNaam);
+    console.log(versienummer);
+    console.log(versiedatum);
+    console.log(autheur);
+    console.log(distributeur)
+    console.log(darkmode)
+    }
+}
+
+let app = new App();
+app.runapplication();
+
